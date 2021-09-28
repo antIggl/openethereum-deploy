a13db8497bfcd79e473e468942392c912a80d15e
Antonios Inglezakis <inglezakis.a@unic.ac.cy>
2021-09-28 17:18:29 +0300

    Added module ethereum-generate-wallet. It's used to replace ethkey for key generation.
    	new file:   .gitmodules
    	new file:   config/utils/ethereum-generate-wallet
    	modified:   config/utils/keygen.sh

--- config/utils/keygen.sh
+++ config/utils/keygen.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 TMPFILE=`mktemp`
 export PATH=$PATH:.
+BASE_PATH=$(dirname $0)
+#if [ ! $(type -P $PWD/ethkey) ];  then
+#    ARCH=`uname -m`
+#    ETHKEY_URL=`curl -sS "https://vanity-service.parity.io/parity-binaries?version=stable&format=markdown&os=linux&architecture=$ARCH" | grep ethkey | awk {'print $5'}  | cut -d"(" -f2 | cut -d")" -f1`
+#    wget -q $ETHKEY_URL
+#    chmod +x ethkey
+#fi
 
-if [ ! $(type -P $PWD/ethkey) ];  then
-    ARCH=`uname -m`
-    ETHKEY_URL=`curl -sS "https://vanity-service.parity.io/parity-binaries?version=stable&format=markdown&os=linux&architecture=$ARCH" | grep ethkey | awk {'print $5'}  | cut -d"(" -f2 | cut -d")" -f1`
-    wget -q $ETHKEY_URL
-    chmod +x ethkey
-fi
-
-if [ ! $(type -P $PWD/ethstore) ];  then
-    ARCH=`uname -m`
-    ETHSTORE_URL=`curl -sS "https://vanity-service.parity.io/parity-binaries?version=stable&format=markdown&os=linux&architecture=$ARCH" | grep ethstore | awk {'print $5'}  | cut -d"(" -f2 | cut -d")" -f1`
-    wget -q $ETHSTORE_URL
-    chmod +x ethstore
-fi
+#if [ ! $(type -P $PWD/ethstore) ];  then
+#    ARCH=`uname -m`
+#    ETHSTORE_URL=`curl -sS "https://vanity-service.parity.io/parity-binaries?version=stable&format=markdown&os=linux&architecture=$ARCH" | grep ethstore | awk {'print $5'}  | cut -d"(" -f2 | cut -d")" -f1`
+#    wget -q $ETHSTORE_URL
+#    chmod +x ethstore
+#fi
 
 
 # Generate the private and public keys
-./ethkey generate random > $TMPFILE
+$BASE_PATH/ethereum-generate-wallet/ethereum-wallet-generator.sh > $TMPFILE
 
-cat $TMPFILE | grep public | awk {'print $2'} > $1/key.pub
-cat $TMPFILE | grep secret | awk {'print $2'} > $1/key.priv
-cat $TMPFILE | grep address | awk {'print $2'} > $1/address.txt
+cat $TMPFILE | grep Public | awk {'print $3'} > $1/key.pub
+cat $TMPFILE | grep Private | awk {'print $3'} > $1/key.priv
+cat $TMPFILE | grep Address | awk {'print $2'} > $1/address.txt
 
 rm -rf $TMPFILE
 
